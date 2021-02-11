5cca771ed856d525075168f9b4e236cd6d56d650
Danni St.Claire-Coe <47538480+danniscoe@users.noreply.github.com>
2021-02-10 22:03:29 -0600

    First commit

---
+++ complete/src/main/java/com/example/validatingforminput/ValidateFormInput.java
@@ -0,0 +1,13 @@
+package com.example.validatingforminput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateFormInput {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ValidateFormInput.class, args);
+	}
+
+}
